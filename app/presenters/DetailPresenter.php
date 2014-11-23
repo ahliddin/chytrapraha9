ebf52187eb97fea4aeb945c4b7ff5bbbe7a6f077
Zdenek Styblik <zdenek.styblik@icflix.com>
2014-11-23 13:02:55 +0100

    Add 'Detail' view
    Commit adds detailed view of given institution as shown on wireframes, page 3.
    There are some TODOs left, though, like:
    * FB integration which requires FB account
    * social network buttons - use www.addthis.com for that?
    * rating - requires graphics and/or 3rd party plugin

---
+++ app/presenters/DetailPresenter.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Presenters;
+
+use Nette,
+    App\Model;
+
+/*
+ * Show detail of given institution.
+ */
+
+class DetailPresenter extends BasePresenter
+{
+    public function renderDefault()
+    {
+    }
+}
