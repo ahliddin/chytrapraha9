991477b9e8d2dca603dc6391af289ce9e455f9ad
Zdenek Styblik <zdenek.styblik@icflix.com>
2014-11-23 09:43:48 +0100

    Add List presenter
    Commit adds a simple presenter of the List view in order to be able to view the
    List template and how it fits in.

---
+++ app/presenters/ListPresenter.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Presenters;
+
+use Nette,
+    App\Model;
+
+/*
+ * List institutions of type X presenter.
+ */
+
+class ListPresenter extends BasePresenter
+{
+    public function renderDefault()
+    {
+    }
+}
