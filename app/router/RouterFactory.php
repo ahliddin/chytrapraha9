82f779e88c5414418bfb074ee97a6703be6ce29d
Zdenek Styblik <zdenek.styblik@icflix.com>
2015-03-16 16:42:40 +0100

    retab in Router

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -3,9 +3,9 @@
 namespace App;
 
 use Nette,
-	Nette\Application\Routers\RouteList,
-	Nette\Application\Routers\Route,
-	Nette\Application\Routers\SimpleRouter;
+    Nette\Application\Routers\RouteList,
+    Nette\Application\Routers\Route,
+    Nette\Application\Routers\SimpleRouter;
 
 
 /**
@@ -14,18 +14,18 @@ use Nette,
 class RouterFactory
 {
 
-	/**
-	 * @return \Nette\Application\IRouter
-	 */
-	public function createRouter()
-	{
-		$router = new RouteList();
-                $router[] = new Route('', 'Homepage:default');
-		$router[] = new Route('index.php', 'Homepage:default');
-                $router[] = new Route('mapa-instituci[/<urlId>]', 'Map:default');
-                $router[] = new Route('<urlId>', 'List:default');
-                $router[] = new Route('<categoryUrlId>/<urlId>', 'Detail:default');
-		return $router;
-	}
+    /**
+     * @return \Nette\Application\IRouter
+     */
+    public function createRouter()
+    {
+        $router = new RouteList();
+        $router[] = new Route('', 'Homepage:default');
+        $router[] = new Route('index.php', 'Homepage:default');
+        $router[] = new Route('mapa-instituci[/<urlId>]', 'Map:default');
+        $router[] = new Route('<urlId>', 'List:default');
+        $router[] = new Route('<categoryUrlId>/<urlId>', 'Detail:default');
+        return $router;
+    }
 
 }
