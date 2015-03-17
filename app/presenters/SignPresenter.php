102c28521ba1355f64eb6d5228dc23da8ba3ecb1
Pavel_2 <Pavel_2@otisk-NTB>
2015-03-17 15:00:14 +0100

    pridana flash message po prihlaseni

--- app/presenters/SignPresenter.php
+++ app/presenters/SignPresenter.php
@@ -3,12 +3,10 @@
 namespace App\Presenters;
 
 use Nette,
-	Nette\Security\Passwords,
-	Nette\Application\UI\Form;
+    Nette\Security\Passwords,
+    Nette\Application\UI\Form;
 
-
-class SignPresenter extends BasePresenter
-{
+class SignPresenter extends BasePresenter {
 
     /**
      * @var \App\Model\UserManager
@@ -16,45 +14,40 @@ class SignPresenter extends BasePresenter
      */
     public $userManager;
 
-	/**
-	 * Sign-in form factory.
-	 * @return Nette\Application\UI\Form
-	 */
-	protected function createComponentSignInForm()
-	{
-		$form = new Form;
-		$form->addText('username', 'Username:')
-			->setRequired('Please enter your username.');
+    /**
+     * Sign-in form factory.
+     * @return Nette\Application\UI\Form
+     */
+    protected function createComponentSignInForm() {
+        $form = new Form;
+        $form->addText('username', 'Username:')
+                ->setRequired('Please enter your username.');
 
-		$form->addPassword('password', 'Password:')
-			->setRequired('Please enter your password.');
+        $form->addPassword('password', 'Password:')
+                ->setRequired('Please enter your password.');
 
-		$form->addSubmit('send', 'Sign in');
+        $form->addSubmit('send', 'Sign in');
 
-		// call method signInFormSucceeded() on success
-		$form->onSuccess[] = array($this, 'signInFormSucceeded');
-		return $form;
-	}
+        // call method signInFormSucceeded() on success
+        $form->onSuccess[] = array($this, 'signInFormSucceeded');
+        return $form;
+    }
 
-
-	public function signInFormSucceeded($form, $values)
-	{
-		try {
-            $this->getUser()->login($values->username,
-                $values->password);
-			$this->redirect('Homepage:');
-
-		} catch (Nette\Security\AuthenticationException $e) {
+    public function signInFormSucceeded($form, $values) {
+        try {
+            $this->getUser()->login($values->username, $values->password);
+            $this->flashMessage('Vaše přihlášení proběhlo úspěšně', 'success');
+            $this->redirect('Homepage:');
+        } catch (Nette\Security\AuthenticationException $e) {
             $form->addError(Passwords::hash($values->password));
-			$form->addError('Incorrect username or password.');
-		}
-	}
+            $form->addError('Incorrect username or password.');
+        }
+    }
 
+    public function actionOut() {
+        $this->getUser()->logout();
+        $this->flashMessage('You have been signed out.');
+        $this->redirect('Homepage:');
+    }
 
-	public function actionOut()
-	{
-		$this->getUser()->logout();
-		$this->flashMessage('You have been signed out.');
-		$this->redirect('Homepage:');
-	}
 }
